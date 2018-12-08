9ee8b0b9c588512b7e888a43c7d22138fb60dcae
Noah Randolph <noah.randolph@gmail.com>
2018-12-07 17:37:09 -0800

    basic RDD one hot encoder created

--- featureEngineering.py
+++ featureEngineering.py
@@ -2,8 +2,9 @@
 
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import udf, desc, isnan, when
-from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer
 import numpy as np
+from operator import add
+import copy
 
 
 MAINCLOUDPATH = 'gs://w261_final_project/train.txt'
@@ -11,6 +12,8 @@ TOYCLOUDPATH = 'gs://w261_final_project/train_005.txt'
 TOYLOCALPATH = 'data/train_005.txt'
 NUMERICCOLS = 13
 CATEGORICALCOLS = 26
+NUMERICCOLNAMES = ['I{}'.format(i) for i in range(0,NUMERICCOLS)]
+CATCOLNAMES = ['C{}'.format(i) for i in range(0,CATEGORICALCOLS)]
 SEED = 2615
 
 
@@ -30,7 +33,7 @@ def loadData():
     df = spark.read.csv(path=TOYLOCALPATH, sep='\t')
     # change column names
     oldColNames = df.columns
-    newColNames = ['Label']+['I{}'.format(i) for i in range(0,NUMERICCOLS)]+['C{}'.format(i) for i in range(0,CATEGORICALCOLS)]
+    newColNames = ['Label'] + NUMERICCOLNAMES + CATCOLNAMES
     for oldName, newName in zip(oldColNames, newColNames):
         df = df.withColumnRenamed(oldName, newName)
     # change int column types to int from string
@@ -90,6 +93,33 @@ def dfToRDD(row):
     return (features_array, y)
 
 
+def emitColumnAndCat(line):
+    """
+    Takes in a row from RDD and emits a record for each categorical column value along with a zero for one-hot encoding.
+    The emitted values will become a reference dictionary for one-hot encoding in later steps.
+        Input: (array([features], dtype='<U21'), 0) or (features, label)
+        Output: ((categorical column, category), 0) or (complex key, value)
+    The last zero in the output is for initializing one-hot encoding.
+    """
+    elements = line[0][NUMERICCOLS:]
+    for catColName, element in zip(CATCOLNAMES, elements):
+        yield ((catColName, element), 0)
+
+
+def oneHotEncoder(line):
+    """
+    Takes in a row from RDD and emits row where categorical columns are replaced with 1-hot encoded columns.
+        Input: (numerical and categorical features, label)
+        Output: (numerical and one-hot encoded categorical features, label)
+    """
+    oneHotDict = copy.deepcopy(oneHotReference)
+    elements = line[0][NUMERICCOLS:]
+    for catColName, element in zip(CATCOLNAMES, elements):
+        oneHotDict[(catColName, element)] = 1
+    numericElements = list(line[0][:NUMERICCOLS])
+    return (numericElements + [value for key, value in oneHotDict.items()], line[1])
+
+
 # load data
 df = loadData()
 testDf, trainDf = splitIntoTestAndTrain(df)
@@ -121,24 +151,41 @@ testDf = testDf.na.fill(fillNADictNum) \
 trainDf = rareReplacer(trainDf, setsMostFreqCatDict) # df gets cached in function
 testDf = rareReplacer(testDf, setsMostFreqCatDict) # df gets cached in function
 
-# numerically index categorical columns for one-hot encoder
-for catColumn in trainDf.columns[NUMERICCOLS+1:]:
-    catIndexer = StringIndexer(inputCol=catColumn, outputCol=catColumn+'Index', handleInvalid='keep') # forces you to have different in and out col names
-    stringIndexerModel = catIndexer.fit(trainDf)
-    trainDf = stringIndexerModel.transform(trainDf).drop(catColumn).cache() # original string columns are kept in dataframe so should be deleted
-    testDf = stringIndexerModel.transform(testDf).drop(catColumn).cache()
+# # numerically index categorical columns for one-hot encoder and to combine rare categories into one
+# for catColumn in trainDf.columns[NUMERICCOLS+1:]:
+#     catIndexer = StringIndexer(inputCol=catColumn, outputCol=catColumn+'Index', handleInvalid='error') # forces you to have different in & out col names
+#     stringIndexerModel = catIndexer.fit(trainDf)
+#     trainDf = stringIndexerModel.transform(trainDf).drop(catColumn).cache() # original string columns are kept in dataframe so should be deleted
+#     testDf = stringIndexerModel.transform(testDf).drop(catColumn).cache()
+    
+# # convert categorical columns to 1 hot encoded columns
+# indexColumnNames = trainDf.columns[NUMERICCOLS+1:]
+# oneHotColumnNames = [column.replace('Index', '') for column in trainDf.columns[NUMERICCOLS+1:]]
+# oneHotEncoder = OneHotEncoderEstimator(inputCols=indexColumnNames, outputCols=oneHotColumnNames) # forces you to have different in & out column names
+# oneHotModel = oneHotEncoder.fit(trainDf)                                                                        
+# trainDf = oneHotModel.transform(trainDf).cache()
+# testDf = oneHotModel.transform(testDf).cache()
 
-# convert categorical columns to 1 hot encoded columns
-indexColumnNames = trainDf.columns[NUMERICCOLS+1:]
-oneHotColumnNames = [column.replace('Index', '') for column in trainDf.columns[NUMERICCOLS+1:]]
-oneHotEncoder = OneHotEncoderEstimator(inputCols=indexColumnNames, outputCols=oneHotColumnNames) # forces you to have different in and out column names
-oneHotModel = oneHotEncoder.fit(trainDf)                                                                        
-trainDf = oneHotModel.transform(trainDf).cache()
-testDf = oneHotModel.transform(testDf).cache()
+# # drop the index columns (original string columns are kept in dataframe so should be deleted)
+# for column in indexColumnNames:
+#     trainDf = trainDf.drop(column) 
+#     testDf = testDf.drop(column)
 
-# drop the index columns (original string columns are kept in dataframe so should be deleted)
-for column in indexColumnNames:
-    trainDf = trainDf.drop(column).cache() 
-    testDf = testDf.drop(column).cache()
+# # convert SparseVectors to 1D arrays in order to convert dataframe to RDD    
+# for column in trainDf.columns[NUMERICCOLS+1:]:
+#     trainDf = trainDf.withColumn(column, udf(lambda x: list(OrderedDict((y, None) for y in x)))(trainDf[column])).cache()
 
-print(trainDf.take(1))
+# convert dataframe to RDD 
+trainRDD = trainDf.rdd.map(dfToRDD).cache()
+testRDD = testDf.rdd.map(dfToRDD).cache()
+        
+# create and broadcast reference dictionary to be used in constructing 1 hot encoded RDD
+oneHotReference = trainRDD.flatMap(emitColumnAndCat) \
+                          .reduceByKeyLocally(add) # note: only the zero values are being added here (main goal is to output a dictionary)
+sc.broadcast(oneHotReference)
+
+# replace rows with new rows having categorical columns 1-hot encoded
+trainRDD1Hot = trainRDD.map(oneHotEncoder)
+
+
+print(trainRDD1Hot.takeSample(False, 5, SEED))
